7ec885d8e04dfae075d7f5892a028414ded9dd67
nacho <nachom@outlook.com.ar>
2023-05-08 18:12:55 -0300

    1.2 course info

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,8 +12,18 @@ const Header = (param) => {
 
 const Content = (param) => {
   return (
-    <p> {param.part} {param.exercises}</p>
+    <div>
+      <Part name= {param.part1} numExercises= {param.exercises1}/>
+      <Part name= {param.part2} numExercises= {param.exercises2}/>
+      <Part name= {param.part3} numExercises= {param.exercises3}/>
+    </div>
     )
+}
+
+const Part = (param) => {
+  return (
+    <p>{param.name} {param.numExercises}</p>
+  )
 }
 
 const Total = (param) => {
@@ -44,9 +54,7 @@ const App = () => {
     <div>
       <Header name={course}/>
       
-      <Content  part={part1} exercises={exercises1} />
-      <Content  part={part2} exercises={exercises2} />
-      <Content  part={part3} exercises={exercises3} />
+      <Content  part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
 
       <Total totalExercises={[exercises1, exercises2, exercises3]}/>
       
