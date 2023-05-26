8ae366d156ab0f9ce2bd11cc14e5e3c314d9da90
nacho <nachom@outlook.com.ar>
2023-05-26 03:04:26 -0300

    exercise 1.9

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,22 @@
 import { useState } from 'react'
 
-const Statistics = ({good, neutral, bad}) => {
-  
-
-  return (
-  <>
-    <h2>Statistics</h2>
-    <p>Good: {good}</p>
-    <p>Neutral: {neutral}</p>
-    <p>Bad: {bad}</p>
-    <p>All: {good + neutral + bad}</p>
-    <p>Average: {(good  - bad) }</p>
-    <p>Positive: {good/(good + neutral + bad) *100 }%</p>
-  </>)
-
-
-}
-
-
+const Statistics = ({ good, neutral, bad }) => {
+  if (good !== 0 || neutral !== 0 || bad !== 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {good + neutral + bad}</p>
+        <p>Average: {good - bad}</p>
+        <p>Positive: {(good / (good + neutral + bad)) * 100}%</p>
+      </>
+    );
+  } else {
+    return <h2>No feedback given</h2>;
+  }
+};
 
 
 const App = () => {
@@ -25,9 +24,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  
-
 
   return (
     <div>
