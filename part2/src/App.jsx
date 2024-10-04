0d8f72faf91a12846af052cf4b0af4687145eb51
nacho <nachom@outlook.com.ar>
2024-10-04 03:48:50 +0200

    exercise 2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import PersonRender from "./components/PersonRender";
+import axios from "axios";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "30-40605080", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	useEffect(() => {
+		axios
+		.get("http://localhost:3001/persons")
+		.then((response) => {
+			setPersons(response.data);
+		});
+	}, []);
+
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [isFiltered, setIsFiltered] = useState(false);
